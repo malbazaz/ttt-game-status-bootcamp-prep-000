0e3457785cb7093f72d84f7c7a7e7ac1301cf088
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-10 05:13:11 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,30 @@
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+  
+def won?(board)
+    WIN_COMBINATIONS.detect do |win_combo|
+board[win_combo[0]]==board[win_combo[1]] && board[win_combo[1]]==board[win_combo[2]] &&( board[win_combo[0]]=="X" || board[win_combo[0]]=="O")
+  
+    end
+  end 
+  
+def full?(board)
+ board.all? do |noxo|
+    noxo="X" || noxo="O"
+  end   
+end 
